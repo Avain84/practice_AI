f5dec02674ecd3b88617b8e7436529ccb385d642
Avain <sports113499@gmail.com>
2024-01-08 12:22:26 +0800

    新增price頁面Section2/新增tailwind extend內容

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      width:{
+        'grid-2item': 'calc((100% - 24px)/2)',
+      },
+    },
     screens: {
       'dt-l' : '1000px',
       'dt' : '768px'
